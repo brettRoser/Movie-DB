05f0600459e333764c76f46a3353857ec4356f7a
brettRoser <brett.roser@yahoo.com>
2019-12-22 00:39:38 -0600

    Added backend api(2)

---
+++ server/MovieApi/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public void Post([FromForm]string username, [FromForm]string password)
+        {
+            Console.WriteLine(username);
+            Console.WriteLine(password);
+        }
+    }
+}
